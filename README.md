20f4f6be0b4a77744e1d72308022fc7ee492e533
Angus.Fenying <i.am.x.fenying@gmail.com>
2017-12-01 19:08:24 +0800

    Removed compiled files.

--- README.md
+++ README.md
@@ -2,6 +2,12 @@
 
 The core of LiteRT.
 
+## Installation
+
+```sh
+npm install @litert/core --save
+```
+
 ## License
 
 This library is published under [Apache-2.0](./LICENSE) license.
--- dist/index.d.ts
+++
@@ -1,8 +0,0 @@
-/**
- * The template type of dictionary.
- */
-export interface IDictionary<T> {
-    [key: string]: T;
-}
-export * from "./class.Exception";
-export * from "./class.RawPromise";
